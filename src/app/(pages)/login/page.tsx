8660ff22373a4abfc616bd15d5c2ade64bffe6a3
Cycle9898 <cycle9898@gmail.com>
2024-11-08 18:58:38 +0100

    implement login page

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -8,26 +8,57 @@ import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import LoginForm from './LoginForm'
 
 import classes from './index.module.scss'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default async function Login() {
   await getMeUser({
-    validUserRedirect: `/account?warning=${encodeURIComponent('You are already logged in.')}`,
+    validUserRedirect: `/account?warning=${encodeURIComponent('Vous êtes déjà connecté.')}`,
   })
 
   return (
-    <Gutter className={classes.login}>
-      <RenderParams className={classes.params} />
-      <h1>Log in</h1>
-      <LoginForm />
-    </Gutter>
+    <section className={classes.login}>
+      <div className={classes.heroImg}>
+        <Link href="/">
+          <Image
+            src="/logo-black.svg"
+            alt="Logo de Tech Heaven"
+            width={250}
+            height={23}
+            className={classes.logo}
+          />
+        </Link>
+      </div>
+
+      <div className={classes.formWrapper}>
+        <div className={classes.formContainer}>
+          <RenderParams className={classes.params} />
+
+          <div className={classes.formTitle}>
+            <h3>Bienvenue</h3>
+
+            <Image
+              src="/assets/icons/hand.png"
+              alt="logo de main qui salue"
+              width={30}
+              height={30}
+            />
+          </div>
+
+          <p>Merci de vous connecter</p>
+
+          <LoginForm />
+        </div>
+      </div>
+    </section>
   )
 }
 
 export const metadata: Metadata = {
-  title: 'Login',
-  description: 'Login or create an account to get started.',
+  title: 'Connexion',
+  description: 'Connectez-vous ou créez un compte pour commencer.',
   openGraph: mergeOpenGraph({
-    title: 'Login',
+    title: 'Connexion',
     url: '/login',
   }),
 }
--- src/app/_components/Input/index.tsx
+++ src/app/_components/Input/index.tsx
@@ -40,7 +40,7 @@ export const Input: React.FC<Props> = ({
             ? {
                 pattern: {
                   value: /\S+@\S+\.\S+/,
-                  message: 'Please enter a valid email',
+                  message: 'Veuillez entrer une adresse e-mail valide',
                 },
               }
             : {}),
@@ -50,7 +50,7 @@ export const Input: React.FC<Props> = ({
       {error && (
         <div className={classes.errorMessage}>
           {!error?.message && error?.type === 'required'
-            ? 'This field is required'
+            ? 'Ce champ est obligatoire'
             : error?.message}
         </div>
       )}
