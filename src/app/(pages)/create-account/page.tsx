b9675a56f8340bd22b71a15c3fec0ea4fbd5e981
Cycle9898 <cycle9898@gmail.com>
2024-11-12 18:02:32 +0100

    implement create account page

--- src/app/(pages)/create-account/page.tsx
+++ src/app/(pages)/create-account/page.tsx
@@ -8,6 +8,8 @@ import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import CreateAccountForm from './CreateAccountForm'
 
 import classes from './index.module.scss'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default async function CreateAccount() {
   await getMeUser({
@@ -17,19 +19,48 @@ export default async function CreateAccount() {
   })
 
   return (
-    <Gutter className={classes.createAccount}>
-      <h1>Create Account</h1>
-      <RenderParams />
-      <CreateAccountForm />
-    </Gutter>
+    <section className={classes.createAccount}>
+      <div className={classes.heroImg}>
+        <Link href="/">
+          <Image
+            src="/logo-black.svg"
+            alt="Logo de Tech Heaven"
+            width={250}
+            height={23}
+            className={classes.logo}
+          />
+        </Link>
+      </div>
+
+      <div className={classes.formWrapper}>
+        <div className={classes.formContainer}>
+          <RenderParams className={classes.params} />
+
+          <div className={classes.formTitle}>
+            <h3>Créer un compte</h3>
+
+            <Image
+              src="/assets/icons/hand.png"
+              alt="logo de main qui salue"
+              width={30}
+              height={30}
+            />
+          </div>
+
+          <p>Merci de renseigner les différents champs</p>
+
+          <CreateAccountForm />
+        </div>
+      </div>
+    </section>
   )
 }
 
 export const metadata: Metadata = {
-  title: 'Account',
-  description: 'Create an account or log in to your existing account.',
+  title: 'Création de compte',
+  description: 'Créez un compte ou connectez-vous à votre compte existant.',
   openGraph: mergeOpenGraph({
-    title: 'Account',
+    title: 'Création de compte',
     url: '/account',
   }),
 }
