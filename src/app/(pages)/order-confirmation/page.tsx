a15195018bd9751ebe15ab04f575cba84d959eb4
Cycle9898 <cycle9898@gmail.com>
2024-12-09 16:07:11 +0100

    implement order confirmation page

--- src/app/(pages)/order-confirmation/page.tsx
+++ src/app/(pages)/order-confirmation/page.tsx
@@ -10,7 +10,7 @@ import classes from './index.module.scss'
 export default async function OrderConfirmation() {
   return (
     <Gutter className={classes.confirmationPage}>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<div>Chargement...</div>}>
         <OrderConfirmationPage />
       </Suspense>
     </Gutter>
@@ -18,8 +18,8 @@ export default async function OrderConfirmation() {
 }
 
 export const metadata: Metadata = {
-  title: 'Order Confirmation',
-  description: 'Your order has been confirmed.',
+  title: 'Confirmation de commande',
+  description: 'Votre commande a été prise en compte.',
   openGraph: mergeOpenGraph({
     title: 'Order Confirmation',
     url: '/order-confirmation',
